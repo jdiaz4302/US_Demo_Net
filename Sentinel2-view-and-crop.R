893402a96479060d01932cfdf7245e5cd16a35d0
jdiaz4302 <jedi4302@colorado.edu>
2018-04-19 20:57:42 -0600

    Cropping and saving raster stacks, proving they can be used in python

--- Sentinel2-view-and-crop.R
+++ Sentinel2-view-and-crop.R
@@ -5,8 +5,10 @@
 library(raster)
 
 
-# Sitting file names - WILL NEED TO CHANGE
-# Denver, CO
+# WHERE TO SET THINGS UP AT
+# City name - for creating output file names
+city_name <- 'Denver'
+# File names - for importing
 B1 <- 'US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20180414T212337.SAFE/GRANULE/L1C_T13TDE_A014681_20180414T174725/IMG_DATA/T13TDE_20180414T173901_B01.jp2'
 B2 <- 'US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20180414T212337.SAFE/GRANULE/L1C_T13TDE_A014681_20180414T174725/IMG_DATA/T13TDE_20180414T173901_B02.jp2'
 B3 <- 'US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20180414T212337.SAFE/GRANULE/L1C_T13TDE_A014681_20180414T174725/IMG_DATA/T13TDE_20180414T173901_B03.jp2'
@@ -19,6 +21,9 @@ B9 <- 'US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20
 B10 <- 'US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20180414T212337.SAFE/GRANULE/L1C_T13TDE_A014681_20180414T174725/IMG_DATA/T13TDE_20180414T173901_B10.jp2'
 B11 <- 'US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20180414T212337.SAFE/GRANULE/L1C_T13TDE_A014681_20180414T174725/IMG_DATA/T13TDE_20180414T173901_B11.jp2'
 B12 <- 'US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20180414T212337.SAFE/GRANULE/L1C_T13TDE_A014681_20180414T174725/IMG_DATA/T13TDE_20180414T173901_B12.jp2'
+# Capital coordinates - for cropping
+northing <- as.integer(4399127.19)
+easting <- as.integer(500725.62)
 
 
 # Import images
@@ -36,17 +41,13 @@ s2_B11 <- raster(B11)
 s2_B12 <- raster(B12)
 
 
-# WILL NEED TO CHAN
-# City coordinates from google (in UTM): https://www.latlong.net/lat-long-utm.html 
-northing <- as.integer(4399127.19)
-easting <- as.integer(500725.62)
-
-
 # Setting the cropping extent so that we get 500x500
-cropping_extent <- extent(easting - 250,
-                          easting + 250,
-                          northing - 250,
-                          northing + 250)
+# 250 pixels in each direction from the center 
+# Times the spatial resolution of 10m
+cropping_extent <- extent(easting - 250*10,
+                          easting + 250*10,
+                          northing - 250*10,
+                          northing + 250*10)
 
 
 # Cropping
@@ -67,17 +68,7 @@ B11_cropped<- raster::crop(s2_B11, new_cropping_extent)
 B12_cropped<- raster::crop(s2_B12, new_cropping_extent)
 
 
-# Visualization
-TCI <- brick('US_Demo_Net/Images/Denver/S2A_MSIL1C_20180414T173901_N0206_R098_T13TDE_20180414T212337.SAFE/GRANULE/L1C_T13TDE_A014681_20180414T174725/IMG_DATA/T13TDE_20180414T173901_TCI.jp2')
-raster::plotRGB(TCI)
-for_plotting <- extent(easting - 5000,
-                       easting + 5000,
-                       northing - 5000,
-                       northing + 5000)
-plot(for_plotting, add = TRUE, col = 'limegreen', lwd = 4)
-
-
-# Stacking bands
+# Stacking bands by like spatial resolutions
 ten_meter_bands <- stack(B2_cropped, B3_cropped, B4_cropped,
                          B8_cropped)
 twenty_meter_bands <- stack(B5_cropped, B6_cropped, B7_cropped,
@@ -86,8 +77,18 @@ sixty_meter_bands <- stack(B1_cropped, B9_cropped, B10_cropped)
 
 
 # Saving stacks
-writeRaster(B2_cropped, "US_Demo_Net/Images/Denver/10_m/b2.tif", format = "GTiff", overwrite = TRUE)
-testing <- raster("US_Demo_Net/Images/Denver/10_m/b2.tif")
-testing <- readAll(testing)
-testing@data@values
-plot(testing)
+# Getting the generic output file path for the city
+filepath <- paste0('US_Demo_Net/Images/', city_name, "/",city_name)
+# Setting a specific file path for the city and the resolution
+filepath10 <- paste0(filepath, '10.tif')
+filepath20 <- paste0(filepath, '20.tif')
+filepath60 <- paste0(filepath, '60.tif')
+# Write the output files
+writeRaster(ten_meter_bands, filepath10,
+            format = "GTiff", overwrite = TRUE)
+writeRaster(twenty_meter_bands, filepath20,
+            format = "GTiff", overwrite = TRUE)
+writeRaster(sixty_meter_bands, filepath60,
+            format = "GTiff", overwrite = TRUE)
+
+
